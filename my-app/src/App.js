be9430c2c0a5c532c7017367578ae97e90e3907a
tbiehl22 <57846653+tbiehl22@users.noreply.github.com>
2020-04-29 16:11:00 -0600

    added files

---
+++ my-app/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => (
+  <div className="container-fluid mainContainer">
+    <Jumbotron />
+    <MainGameContainer /> 
+  </div>
+);
+
+export default App;
