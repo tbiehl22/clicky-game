4be3e7fe038ee5761949f68d408d17b2629ba9ea
tbiehl22 <57846653+tbiehl22@users.noreply.github.com>
2020-05-01 15:53:45 -0600

    Updates

---
+++ precache-manifest.ee0ce12c2b85ca57c7e38598005f87c2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75711dfde4d2e91ce30e578bb6dc219e",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "fd32a081d7f40be4f3fa",
+    "url": "/clicky-game/static/css/main.a7ec3ae7.chunk.css"
+  },
+  {
+    "revision": "1798f8632b49ff162e86",
+    "url": "/clicky-game/static/js/2.4cff54be.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/clicky-game/static/js/2.4cff54be.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd32a081d7f40be4f3fa",
+    "url": "/clicky-game/static/js/main.525d4946.chunk.js"
+  },
+  {
+    "revision": "cd4f51ed28cba516d90b",
+    "url": "/clicky-game/static/js/runtime-main.2c0e42e5.js"
+  }
+]);
